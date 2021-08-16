e7aad3fe07b3c2848e1af8296f1a7bfd72834575
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2021-08-16 17:22:57 +0200

    feat: installable

---
+++ frontend/public/service-worker.js
@@ -0,0 +1,12 @@
+
+self.addEventListener('install', function (event) {
+    console.log('installed', event);
+});
+
+self.addEventListener('activate', function (event) {
+    console.log('activated', event);
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log('Handling fetch event for', event.request.url);
+});
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("/service-worker.js").then(
+      function (registration) {
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function (err) {
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
